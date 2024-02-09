858c51e4ec2149a36620fc2130c9cac8f7df9b85
Vlada <vlada.shevchenko.it@gmail.com>
2024-02-09 22:55:15 +0200

    corrected showResult

--- Main.py
+++ Main.py
@@ -8,59 +8,60 @@ score  = 0
 preyScore = 0
 preyTimer = 0
 preyCount = []
+preyMaxCount = 12
 
 timer = pygame.time.Clock()
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("DuckHunt")
 
 STARTBG = pygame.image.load("images/startmenu.png")
 LAYERBG = pygame.image.load("images/background.png")
 
-
-
-def showResult(text, score):
+def showResult(text, score, preys):
     run = True
-    PLAY_TEXT = pygame.font.Font("assets/VCR_OSD_MONO_1.001.ttf", 45).render(text + " Score: " + str(score), True, "White")
-    PLAY_RECT = PLAY_TEXT.get_rect(center=(500, 260))
-
-
-    # Визначаємо розміри контура
-    stroke_width = 2
-
-    # Намальований прямокутник з контуром та заповненням
-    border_rect = PLAY_RECT.inflate(stroke_width, stroke_width)
+    text_render = pygame.font.Font("assets/VCR_OSD_MONO_1.001.ttf", 45).render(text, True, "White")
+    score_render = pygame.font.Font("assets/VCR_OSD_MONO_1.001.ttf", 45).render("Score: " + str(score), True, "White")
+    pray_render = pygame.font.Font("assets/VCR_OSD_MONO_1.001.ttf", 45).render("Ducks: " + str(preys) + " from "+ str(preyMaxCount), True, "White")
     
-    # Відображаємо заповнений прямокутник
-    pygame.draw.rect(screen, "Black", border_rect)  # Заповнений прямокутник
+    text_loc = text_render.get_rect(center=(500, 200))
+    score_loc = score_render.get_rect(center=(500, 250))
+    pray_loc = pray_render.get_rect(center=(500, 300))
 
-    # Відображаємо контур прямокутника
-    pygame.draw.rect(screen, "White", border_rect, stroke_width)  # Контур прямокутника
-
-    # Відображаємо текст у центрі прямокутника
-    screen.blit(PLAY_TEXT, PLAY_RECT)
-    
     while run:
+        # show black rectangle behind text
+        pygame.draw.rect(screen, (0, 0, 0), (250, 150, 500, 225))
+
+        # show text
+        screen.blit(text_render, text_loc)
+        screen.blit(score_render, score_loc)
+        screen.blit(pray_render, pray_loc)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
                 run = False
-            pygame.display.flip()
+        pygame.display.flip()
     pygame.quit()                                
-    
 
 def showGameUi():
     run = True
     
-    
     while run:
-        screen.fill(pygame.Color('#3FBFFE'))
         timer.tick(FPS)
-        screen.blit(LAYERBG,(0,0))
         
+        # fill down layer of screen with color
+        screen.fill(pygame.Color('#3FBFFE'))
+        # upper layer with grass and UI
+        screen.blit(LAYERBG,(0,0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                
+            # show result depends on preyScore
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    showResult(" You win! ", score)
-                      
+                    showResult(" You win! ", score, preyScore)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                    showResult(" Oh no... You lose! ", score, preyScore)          
         pygame.display.flip()        
     pygame.quit()     
 
@@ -73,7 +74,8 @@ def startMenu():
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     showGameUi()  
-                    
+        
+        #show start menu bg
         screen.blit(STARTBG,(0,0))
         pygame.display.flip()        
     pygame.quit()     
