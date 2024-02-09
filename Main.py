9f2af9632d5987d574a704f0471af5ff74ba6e76
Vlada <vlada.shevchenko.it@gmail.com>
2024-02-09 21:27:42 +0200

    add start_menu and play functions

--- Main.py
+++ Main.py
@@ -4,18 +4,39 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 1000
 FPS = 60
-timer = pygame.time.Clock
+timer = pygame.time.Clock()
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+STARTBG = pygame.image.load("images/startmenu.png")
+LAYERBG = pygame.image.load("images/background.png")
 
-def main():
+def play():
     run = True
+    
     while run:
+        screen.fill(pygame.Color('#3FBFFE'))
+        timer.tick(FPS)
+        screen.blit(LAYERBG,(0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+        pygame.display.update()        
     pygame.quit()     
 
+def start_menu():
+    run = True
+    
+    while run:  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    play()  
+                    
+        screen.blit(STARTBG,(0,0))
+        pygame.display.flip()        
+    pygame.quit()     
 
 if __name__ == "__main__":
-    main()
+    start_menu()
+    
