66902ec2d04b743d80add8cb6a81f644663bad4f
luiqor <vohnenna@gmail.com>
2024-02-11 10:51:32 +0200

    object constructor added

---
+++ object.py
@@ -0,0 +1,11 @@
+import pygame
+class Object():
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.velocity = [0,0]
+    
+
