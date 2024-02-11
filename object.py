fa653373fa22d70d085ef9679a1edf09ea2fe344
luiqor <vohnenna@gmail.com>
2024-02-11 10:58:32 +0200

    update and draw methods added

--- object.py
+++ object.py
@@ -1,5 +1,6 @@
 import pygame
 class Object():
+    gameScreen = None
     def __init__(self, x, y, width, height, image):
         self.x = x
         self.y = y
@@ -7,5 +8,13 @@ class Object():
         self.height = height
         self.image = image
         self.velocity = [0,0]
+
+    def update(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
+        self.draw()
+
+    def draw(self):
+        self.gameScreen.blit(pygame.transform.scale(self.image, (self.width, self.height)), (self.x, self.y))
     
 
