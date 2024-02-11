e8bbbb38ef5e259d1041b47637c229d7e87cf006
luiqor <vohnenna@gmail.com>
2024-02-11 19:17:22 +0200

    pray move method added

--- pray.py
+++ pray.py
@@ -1,8 +1,11 @@
 import pygame
 from object import Object
-
+from random import randint
+import random
 class Pray(Object):
     animationFramerate = 12
+    minVelocity = 4
+    maxVelocity = 8
     horizontalFlyRow = 1 #row in tileset
     upFlyRow = 0
 
@@ -15,6 +18,12 @@ class Pray(Object):
         self.frames = [0, 1, 2, 1]
         self.frame_timer = 0
         self.velocity = [0, 0]
+
+    def update(self):
+            self.move()
+            self.x += self.velocity[0]
+            self.y += self.velocity[1]
+            self.draw()
 
     def load_tileset(self, filename, width, height):
         image = pygame.image.load(filename).convert_alpha()
@@ -52,7 +61,22 @@ class Pray(Object):
         self.flipX = False
         self.direction =  self.horizontalFlyRow
         #logic depending on velocity to change direction
+    
+    def move(self):
+        # Changing the value if speed_x and speed_y both are zero
+        if self.velocity == [0, 0]:
+            self.velocity = [randint(self.minVelocity, self.maxVelocity) * self.direction, randint(self.minVelocity, self.maxVelocity) * self.direction]
+        # Changing the direction and x,y coordinate of the object if the coordinate of left side is less..or right side coordinate is greater..
+        if self.x <= 20 or self.x + self.width >= 990:
+            self.move_direction = -1 if self.x + self.width >= 990 else 1
+            self.velocity[0] = random.choice([0, randint(self.minVelocity, self.maxVelocity) ]) * self.move_direction
+            self.velocity[1] = random.choice([0, randint(self.minVelocity, self.maxVelocity) ]) * self.move_direction
         
+        # Changing the direction and x,y coordinate of the object if the coordinate of top side is less.. or bottom side coordinate is greater..
+        if self.y <= 20 or self.y + self.height >= 600:
+            self.move_direction = -1 if self.y + self.height >= 600 else 1
+            self.velocity[0] = random.choice([0, randint(self.minVelocity, self.maxVelocity) ]) * self.move_direction
+            self.velocity[1] = random.choice([0, randint(self.minVelocity, self.maxVelocity) ]) * self.move_direction
 
 
 
