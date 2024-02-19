12097b07605513cf7ce866323205099bbf2c7d19
luiqor <vohnenna@gmail.com>
2024-02-19 22:33:59 +0200

    Hunter class fields and init atributes optimised

--- hunter.py
+++ hunter.py
@@ -1,29 +1,29 @@
 import pygame
+from random import choice
 from constants import SCREEN
 from object import Object
-from constants import TIMER, FPS
 class Hunter(Object):
     animationFramerate = 12
     defaultX = 400
-    defaultY = 580
+    defaultY = 590
+    catchFrame = 0
+    laughFrames = [1, 2]
     
-    def __init__(self, width, height, tileset, direction=1):
+    def __init__(self, width, height, tileset):
         super().__init__(self.defaultX, self.defaultY, width, height, None)
         self.tileset = self.load_tileset(tileset, width, height)
-        self.direction = direction
-        self.flipX = False
+        self.flipX = choice([True,False])
         self.frame = 0
-        self.frames = [0] 
         self.frame_timer = 0
-        self.delay_timer = 0
         self.velocity = [0, 0]
         self.going_down = False
         
     def update(self, state):
+        self.velocity[1] = 2
         self.draw(state)
         yTemp = self.y
 
-        if self.y <= 480:
+        if self.y <= (self.defaultY-100):
             self.going_down = True 
 
         elif self.y > yTemp:  
@@ -31,37 +31,33 @@ class Hunter(Object):
               
 
         if self.going_down:
-            self.y += 2  
+            self.y += self.velocity[1]  
         else:
-            self.y -= 2
+            self.y -= self.velocity[1]  
         
     def load_tileset(self, filename, width, height):
         
-            image = pygame.image.load(filename).convert_alpha()
-            image_width, image_height = image.get_size()    
-            tileset = []
-            line = []  
-            for tile_x in range(0, image_width // width):
-                rect = (tile_x * width, 0, width, height) 
-                line.append(image.subsurface(rect)) 
-            tileset.append(line)  #
-            return tileset
+        image = pygame.image.load(filename).convert_alpha()
+        image_width, image_height = image.get_size()    
+        tileset = []
+        line = []  
+        for tile_x in range(0, image_width // width):
+            rect = (tile_x * width, 0, width, height) 
+            line.append(image.subsurface(rect)) 
+        tileset.append(line)  
+        return tileset
   
     
     def draw(self, state):
         if state == "catch":
-            self.frames = [0] 
-            image_index = self.frames[0]
-            image = pygame.transform.scale(self.tileset[0][image_index], (self.width, self.height))
+            image = pygame.transform.scale(self.tileset[0][self.catchFrame], (self.width, self.height))
             SCREEN.blit(pygame.transform.flip(image, self.flipX, False), (self.x, self.y))
             
-        if state == "laughing":
-            self.frames = [1, 2] 
-
-            if self.frame >= len(self.frames):
+        if state == "laughing": 
+            if self.frame >= len(self.laughFrames):
                 self.frame = 0
 
-            image_index = self.frames[self.frame]  
+            image_index = self.laughFrames[self.frame]  
             image = pygame.transform.scale(self.tileset[0][image_index], (self.width, self.height))
             SCREEN.blit(pygame.transform.flip(image, self.flipX, False), (self.x, self.y))
 
@@ -69,18 +65,14 @@ class Hunter(Object):
             if self.frame_timer < self.animationFramerate:
                 return
             self.frame += 1
-            if self.frame >= len(self.frames):
+            if self.frame >= len(self.laughFrames):
                 self.frame = 0
             self.frame_timer = 0  
                 
     def initialState(self):
         self.x = self.defaultX
         self.y = self.defaultY
-        self.direction = 1
-        self.flipX = False
+        self.flipX = choice([True,False])
         self.frame = 0
-        self.frames = [0] 
         self.frame_timer = 0
-        self.delay_timer = 0
-        self.velocity = [0, 0]
         self.going_down = False
