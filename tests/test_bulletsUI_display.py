59e9bbd0435ec01b7e5de16e522bcc9493213923
Vlada <vlada.shevchenko.it@gmail.com>
2024-03-21 17:51:36 +0200

    Corrected tests bulletsUI and handle_vents

--- tests/test_bulletsUI_display.py
+++ tests/test_bulletsUI_display.py
@@ -1,4 +1,3 @@
-import pygame
 import pytest
 from Main import bulletsUI, SCREEN, FONT
 from unittest.mock import patch
@@ -6,21 +5,14 @@ from unittest.mock import patch
 
 @pytest.mark.parametrize(
     "bulletsCount", [0, 1, 5, 10])
-
-
 def test_bulletsUI_display(bulletsCount):
-
     with patch("main.renderText") as mocked_renderText:
-        
+
         bulletsUI(bulletsCount)
         expected_text = "R = " + str(bulletsCount)
-        
+
         mocked_renderText.assert_called_once()
         args, _ = mocked_renderText.call_args
-        
-        assert args[0:] == (SCREEN, FONT, expected_text, 25, "White", (135, 790))
-        
-        
-        
-        
-        
+
+        assert args[0:] == (SCREEN, FONT, expected_text,
+                            25, "White", (135, 790))
