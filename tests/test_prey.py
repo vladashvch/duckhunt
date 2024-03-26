7ee195d0ab813b336e67f35a7feaca8e651c4f69
luiqor <vohnenna@gmail.com>
2024-03-26 16:19:11 +0200

    test_move_moveDirection added

--- tests/test_prey.py
+++ tests/test_prey.py
@@ -1,5 +1,5 @@
-
 import pytest
+from constants import BOUNDS_X, BOUNDS_Y
 
 
 #  horizontalFlyRow = 1  upFlyRow = 0
@@ -26,3 +26,27 @@ def test_change_direction(prey_fixture, horizontal_velocity, vertical_velocity,
 
     assert prey_fixture.direction == expected_direction
     assert prey_fixture.flipX == expected_flipX
+
+
+# BOUNDS_X = (20, 990) BOUNDS_Y = (20, 600)
+@pytest.mark.parametrize(
+    "moveDirection, x, y, height, width, expected_moveDirection",
+    [
+        (-1, BOUNDS_X[0], 30, 10, 10, 1),  # x at lower bound
+        (1, BOUNDS_X[1] - 50, 30, 50, 50, -1),  # x at upper bound
+        (-1, 30, BOUNDS_Y[0], 30, 30, 1),  # y at lower bound
+        (1, 30, BOUNDS_Y[1] - 10, 10, 10, -1),  # y at upper bound
+        (1, 80, 80, 60, 50, 1),
+    ]
+)
+def test_move_moveDirection(moveDirection, prey_fixture, x, y, height, width,
+                            expected_moveDirection):
+    prey_fixture.moveDirection = moveDirection
+    prey_fixture.x = x
+    prey_fixture.y = y
+    prey_fixture.height = height
+    prey_fixture.width = width
+
+    prey_fixture.move()
+
+    assert prey_fixture.moveDirection == expected_moveDirection
